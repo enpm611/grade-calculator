f1c5fa6b353bcfb589b3d12c76b02058616decc3
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-09-11 17:38:04 -0400

    Min avg points function semicolon remove

--- app/run.py
+++ app/run.py
@@ -30,5 +30,5 @@ optimistic_percentage_grade = GradeCalculator.calculate_optimistic_course_percen
 optimistic_letter_grade = GradeCalculator.calculate_letter_grade(optimistic_percentage_grade)
 print(f'If all other assignments are 100%, the overall course would be {optimistic_percentage_grade*100}%, which is a {optimistic_letter_grade}')
 
-GradeCalculator.minavgpoints();
+GradeCalculator.minavgpoints()
 
